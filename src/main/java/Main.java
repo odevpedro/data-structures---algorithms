45f372a6f9c8587bfa65470b1db8991b5b2e14c2
odevpedro <pedrosschmidt2@gmail.com>
2023-01-17 10:26:46 -0300

    array declaration and concepts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,43 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        //one way to declare the array
+        int[] numbers = new int [3];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+
+        System.out.println(Arrays.toString(numbers));
+
+        //another way
+        int[] sequence = {10, 20, 30};
+        System.out.println(sequence.length); //we cannot change de size of this array
+        System.out.println(Arrays.toString(sequence));
+
+        //and another
+        Array number = new Array(3);
+        number.insert(10);
+        number.insert(10);
+        number.insert(10);
+        number.insert(10);
+        number.removeAT(10);
+        System.out.println(number.indexOf(100));
+
+
+
+
+
+        // São estaticos, possuem um tamanho fixo
+        //Array é a estrutura de dados mais simples, usada para alocar uma lista de itens de forma sequêncial.
+        //A complexidade da busca de um array é O(1)
+        // arrays são estaticos, criados com um tamanho que não pode ser modificado
+
+
+        //A restruturação de um array é proporcional ao tamanho do outro
+
+        /*Quando apagamos um item de um array todos os outros itens são rearanjados de modo a cobrir
+        * o espaço que era ocupado pelo anterior*/
     }
 }
